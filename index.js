a890431753f7b7f736c81cac22f056001f60e09e
JThilinaDK123 <JThilinaDK123@gmail.com>
2023-04-15 11:01:49 +0530

    git-actions pipeline update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thilina" }');
+    res.send('{ "response": "Hello From Thilina9718" }');
 });
 
 app.get('/will', function (req, res) {
